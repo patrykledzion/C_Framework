566bc6c9989ea678972052b38774a6961d69aba8
ssuchy00 <patrykplaypl8@gmail.com>
2025-08-13 21:45:42 +0200

    [VECTOR] added push, pop and working destructor
    added initial size to struct definition

--- main.c
+++ main.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include "Utils/alloc_monitor/my_alloc.h"
-#include "XMLReader/XMLReader.h"
 #include "Utils/Utils.h"
-#include "classes/File/File.h"
+#include "classes/Utils/Vector/Vector.h"
+
 
 int main(void)
 {
-	File file;
-	file_open(&file, "src/test.xml");
-	file_delete(&file);
+	set_alloc_monitor(1);
+	Vector vec;
+	vector_init(&vec, 4);
+
+	for (int i = 0; i < 9; i++)
+	{
+		vector_push(&vec, i * i);
+		printf("%d", (int)vec.data[i]);
+		int x = 2137;
+	}
+
+	printf("\n");
+
+	for (int i = 0; i < 9; i++)
+	{
+		int c = vector_pop(&vec);
+		printf("%d", c);
+		int x = 2137;
+	}
+
+	vector_delete(&vec);
+
+	detect_leaks();
+
 	return 0;
 }
